5b7b3e2b2d4398c2ea898c4678fac74437927fde
Biplap Sarkar <biplap.sarkar@wepay.com>
2016-03-13 23:52:09 -0700

    updated download link

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author='Biplap Sarkar',
   author_email='biplap.sarkar@gmail.com',
   url='https://github.com/biplap-sarkar/pylimit',
-  download_url='https://github.com/biplap-sarkar/pylimit',
+  download_url='https://github.com/biplap-sarkar/pylimit/archive/v0.1.0-beta.tar.gz',
   keywords=['rate limiting', 'throttle', 'redis'],
   classifiers=[],
 )
