c573324a09fa75494dd63fe2433a62ebb1b22147
Biplap Sarkar <biplap.sarkar@wepay.com>
2016-03-13 23:52:58 -0700

    updated version to beta

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pylimit',
   packages = ['pylimit'],
-  version = '0.1.0',
+  version = '0.1.0-beta',
   description = 'A random test lib',
   author='Biplap Sarkar',
   author_email='biplap.sarkar@gmail.com',
