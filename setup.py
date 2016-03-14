43add261eb6a8c626b53cb0be03d9b2c697d166b
Biplap Sarkar <biplap.sarkar@wepay.com>
2016-03-13 23:47:58 -0700

    added license and setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pylimit',
+  packages = ['pylimit'],
+  version = '0.1.0',
+  description = 'A random test lib',
+  author='Biplap Sarkar',
+  author_email='biplap.sarkar@gmail.com',
+  url='https://github.com/biplap-sarkar/pylimit',
+  download_url='https://github.com/biplap-sarkar/pylimit',
+  keywords=['rate limiting', 'throttle', 'redis'],
+  classifiers=[],
+)
