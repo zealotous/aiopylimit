8bd008c93cb98e5b30dee2927b0a0dcfaeabd692
David <markey@rapidratings.com>
2018-03-19 18:46:36 +0000

    Try scm_version this time with setuptools

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
   name = 'aiopylimit',
   packages = ['aiopylimit'],
   description = 'A distributed rate limiting library for python using leaky bucket algorithm and Redis(asyncio version)',
   author='David Markey',
-  version='0.1.1',
   author_email='david@dmarkey.com',
+  use_scm_version=True,
+  setup_requires=['setuptools_scm'],
   url='https://github.com/dmarkey/aiopylimit',
+  install_requires=[
+      'aioredis<=1.1.0',
+  ],
   keywords=['rate limiting', 'throttle', 'redis', 'asyncio'],
   classifiers=[],
 )
