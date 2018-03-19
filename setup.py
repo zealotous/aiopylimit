265f7c7820c00e13dafe68d9e951b14cec305f1a
David <markey@rapidratings.com>
2018-03-19 18:16:37 +0000

    Rip out scm_version as it doesnt work with travis_ci

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ setup(
   name = 'aiopylimit',
   packages = ['aiopylimit'],
   description = 'A distributed rate limiting library for python using leaky bucket algorithm and Redis(asyncio version)',
-  use_scm_version=True,
   author='David Markey',
-  setup_requires=['setuptools_scm'],
+  version='0.1.1',
   author_email='david@dmarkey.com',
   url='https://github.com/dmarkey/aiopylimit',
-  keywords=['rate limiting', 'throttle', 'redis'],
+  keywords=['rate limiting', 'throttle', 'redis', 'asyncio'],
   classifiers=[],
 )
