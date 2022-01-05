0f93a06e751b97959835187a05311deaffaed9d8
Mike Feoktistov <mike.feoktistov@rapidratings.com>
2022-01-05 20:27:58 +0000

    Upgrade library to python 3.9 and the latest compatible versions of aioredis

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 from setuptools import setup
+
 setup(
-  name = 'aiopylimit',
-  packages = ['aiopylimit'],
-  description = 'A distributed rate limiting library for python using leaky bucket algorithm and Redis(asyncio version)',
-  author='David Markey',
-  author_email='david@dmarkey.com',
-  use_scm_version=True,
-  setup_requires=['setuptools_scm'],
-  url='https://github.com/dmarkey/aiopylimit',
-  install_requires=[
-      'aioredis<=1.1.0',
-  ],
-  keywords=['rate limiting', 'throttle', 'redis', 'asyncio'],
-  classifiers=[],
+    name='aiopylimit',
+    packages=['aiopylimit'],
+    description=(
+        'A distributed rate limiting library for python using leaky '
+        'bucket algorithm and Redis(asyncio version)'
+    ),
+    author='David Markey',
+    author_email='david@dmarkey.com',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    url='https://github.com/dmarkey/aiopylimit',
+    install_requires=[
+        'aioredis<2',
+    ],
+    extras_require={
+        "hiredis": ["hiredis<3"],
+    },
+    keywords=['rate limiting', 'throttle', 'redis', 'asyncio'],
+    classifiers=[],
 )
